5922744e3c251383117a4a8ec1e97f59b67f8a18
EnolaZhu <enola0314@gmail.com>
2021-10-28 11:40:55 +0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Enola Zhu on 2021/10/28.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
